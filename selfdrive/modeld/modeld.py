4f14ac48c4f49d1a64f7aeddbe8b12b5706e16f3
Bruce Wayne <harald.the.engineer@gmail.com>
2024-09-22 15:50:36 -0700

    Seems to work

--- selfdrive/modeld/modeld.py
+++ selfdrive/modeld/modeld.py
@@ -30,7 +30,7 @@ SEND_RAW_PRED = os.getenv('SEND_RAW_PRED')
 
 
 MODEL_PATH = Path(__file__).parent / 'models/supercombo.onnx'
-MODEL_PKL_PATH = Path(__file__).parent / 'models/supercombo.pkl'
+MODEL_PKL_PATH = Path(__file__).parent / 'models/supercombo_tinygrad.pkl'
 METADATA_PATH = Path(__file__).parent / 'models/supercombo_metadata.pkl'
 
 class FrameMeta:
